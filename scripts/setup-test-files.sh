51651fc904c1b35dd767057f41af8481b23e7b2d
Devin Stein <devstein@seas.upenn.edu>
2020-01-11 13:54:09 -0800

    Add gpg import to test script

--- scripts/setup-test-files.sh
+++ scripts/setup-test-files.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
+
 # EXPECTS .sops.yaml TO SPECIFY CREATION RULES
-echo "Generating test files..."
 
 TEST_DIR="test/"
 SECRET="secret"
 YAML_SUFFIX=".yaml"
 ENC_SUFFIX=".enc"
 
+echo "Verifying PGP key is imported..."
+gpg --import ${TEST_DIR}/key.asc
+
+echo "Generating test files..."
 sops -e ${TEST_DIR}${SECRET}.yaml > ${TEST_DIR}${SECRET}${ENC_SUFFIX}${YAML_SUFFIX}
 sops -e ${TEST_DIR}${SECRET}-A.yaml > ${TEST_DIR}${SECRET}-A${ENC_SUFFIX}${YAML_SUFFIX}
 sops -e ${TEST_DIR}${SECRET}-B.yaml > ${TEST_DIR}${SECRET}-B${ENC_SUFFIX}${YAML_SUFFIX}
