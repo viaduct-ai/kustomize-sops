61ce22fb412f910250be5cb59099131ab68097ee
Devin Stein <devstein@seas.upenn.edu>
2020-04-05 10:48:41 -0700

    Generator Options via Annotations (#26)
    * Support kustomize behavior annotations
    
    * Add generator options to the README
    
    * Add new test files to setup script

--- ksops_test.go
+++ ksops_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	kusttest_test "sigs.k8s.io/kustomize/api/testutils/kusttest"
+	"sigs.k8s.io/kustomize/api/types"
 )
 
 /*
@@ -101,4 +102,60 @@ func TestKSOPSPluginMultipleResources(t *testing.T) {
 	m := th.LoadAndRunGenerator(string(generatorResource))
 
 	th.AssertActualEqualsExpected(m, string(decryptedResource))
+}
+
+func TestKSOPSPluginHashAnnotation(t *testing.T) {
+	th := kusttest_test.MakeEnhancedHarness(t)
+
+	defer th.Reset()
+
+	th.BuildGoPlugin(
+		kustomizePluginOwner, kustomizePluginVersion, kustomizePluginName)
+
+	th.ResetLoaderRoot(pluginTestDir)
+
+	// Load files from testing directory
+	encryptedResource, err := ioutil.ReadFile(path.Join(localTestDir, "hash", encryptedResourceFile))
+	check(err)
+
+	generatorResource, err := ioutil.ReadFile(path.Join(localTestDir, "hash", "generate-resources.yaml"))
+	check(err)
+
+	// Write encrypt file to make it available to the test harness
+	th.WriteF(path.Join(pluginTestDir, encryptedResourceFile), string(encryptedResource))
+
+	m := th.LoadAndRunGenerator(string(generatorResource))
+
+	if m.Resources()[0].NeedHashSuffix() != true {
+		t.Errorf("expected resource to need hashing")
+	}
+}
+
+func TestKSOPSPluginBehaviorAnnotation(t *testing.T) {
+	th := kusttest_test.MakeEnhancedHarness(t)
+
+	defer th.Reset()
+
+	th.BuildGoPlugin(
+		kustomizePluginOwner, kustomizePluginVersion, kustomizePluginName)
+
+	th.ResetLoaderRoot(pluginTestDir)
+
+	// Load files from testing directory
+	encryptedResource, err := ioutil.ReadFile(path.Join(localTestDir, "behaviors", encryptedResourceFile))
+	check(err)
+
+	generatorResource, err := ioutil.ReadFile(path.Join(localTestDir, "behaviors", "generate-resources.yaml"))
+	check(err)
+
+	// Write encrypt file to make it available to the test harness
+	th.WriteF(path.Join(pluginTestDir, encryptedResourceFile), string(encryptedResource))
+
+	m := th.LoadAndRunGenerator(string(generatorResource))
+
+	resource := m.Resources()[0]
+
+	if resource.Behavior() != types.BehaviorReplace {
+		t.Errorf("expected resource to have behavior %d (replace), but has %d", types.BehaviorReplace, resource.Behavior())
+	}
 }
