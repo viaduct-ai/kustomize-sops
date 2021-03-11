89c1002874aa06d0d54c2c80d3f71b8b25a1ae7c
Devin Stein <devstein@seas.upenn.edu>
2021-03-11 08:37:41 -0800

    Pin to Kustomize v4.0.5

--- scripts/install-kustomize.sh
+++ scripts/install-kustomize.sh
@@ -5,17 +5,18 @@ KUSTOMIZE="kustomize"
 
 function install_kustomize() {
   echo "Installing $KUSTOMIZE..."
-  KUSTOMIZE_VERSION='v3.10.0'
+  KUSTOMIZE_MAJOR_VERSION='v4'
+  KUSTOMIZE_VERSION="${KUSTOMIZE_MAJOR_VERSION}.0.5"
   BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
   KSOPS_VERSION=$(git rev-parse HEAD)
   KSOPS_TAG=$(git describe --exact-match --tags HEAD 2>/dev/null || true )
   LDFLAGS="-X sigs.k8s.io/kustomize/api/provenance.buildDate=${BUILD_DATE}"
-  LDFLAGS+=" -X sigs.k8s.io/kustomize/api/provenance.gitCommit=v3@${KUSTOMIZE_VERSION}"
+  LDFLAGS+=" -X sigs.k8s.io/kustomize/api/provenance.gitCommit=${KUSTOMIZE_MAJOR_VERSION}@${KUSTOMIZE_VERSION}"
   if [ ! -z $KSOPS_TAG ]; then
     KSOPS_VERSION=$KSOPS_TAG
   fi
   LDFLAGS+=" -X sigs.k8s.io/kustomize/api/provenance.version=${KUSTOMIZE_VERSION}+ksops.${KSOPS_VERSION}"
-  GO111MODULE=on go get -ldflags "${LDFLAGS}" sigs.k8s.io/kustomize/kustomize/v3@$KUSTOMIZE_VERSION
+  GO111MODULE=on go get -ldflags "${LDFLAGS}" sigs.k8s.io/kustomize/kustomize/$KUSTOMIZE_MAJOR_VERSION@$KUSTOMIZE_VERSION
 
   echo "Successfully installed $KUSTOMIZE!"
   kustomize version
