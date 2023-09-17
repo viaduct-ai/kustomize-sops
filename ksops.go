e9cdb5e4295b052faac36a5af3e93630a457d3f8
Devin Stein <devstein@seas.upenn.edu>
2023-09-17 16:49:58 -0700

    chore: upgrade to SOPS v3.8.0

--- ksops.go
+++ ksops.go
@@ -18,8 +18,8 @@ import (
 
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
 	"github.com/joho/godotenv"
-	"go.mozilla.org/sops/v3/cmd/sops/formats"
-	"go.mozilla.org/sops/v3/decrypt"
+	"github.com/getsops/sops/v3/cmd/sops/formats"
+	"github.com/getsops/sops/v3/decrypt"
 	"sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/yaml"
 )
